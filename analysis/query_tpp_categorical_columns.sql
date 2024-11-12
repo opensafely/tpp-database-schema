1c6f2bc60783a66e76f450d9b57543e12317b85c
Tom Ward <tomward@fmail.co.uk>
2024-11-12 14:20:21 +0000

    Merge core & tpp categorical columns
    * now that we are using TPP names, there's no need to keep them separate

--- analysis/query_tpp_categorical_columns.sql
+++ analysis/query_tpp_categorical_columns.sql
@@ -1,6 +1,23 @@
 -- This query is for a data development project that doesn't include T1OO data.
--- Consequently, this query doesn't reference the AllowedPatientsWithTypeOneDissent table.
+-- Consequently, this query doesn't reference the
+-- AllowedPatientsWithTypeOneDissent table.
 WITH categorical_columns AS (
+    -- patients
+    SELECT DISTINCT
+        'Patient' AS table_name,
+        'Sex' AS column_name,
+        Sex AS column_value
+    FROM Patient
+    UNION ALL
+
+    -- ons_deaths
+    SELECT DISTINCT
+        'ONS_Deaths' AS table_name,
+        'Place_of_occurrence' AS column_name,
+        Place_of_occurrence AS column_value
+    FROM ONS_Deaths
+    UNION ALL
+
     -- addresses
     SELECT DISTINCT
         'PatientAddress' AS table_name,
